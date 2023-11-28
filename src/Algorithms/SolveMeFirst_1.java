71f7394b5a072a33d12b94d3c43e50d37672713c
mrfoott <phuvo2105@gmail.com>
2023-11-28 18:30:45 +0700

    Initial commit

---
+++ src/Algorithms/SolveMeFirst_1.java
@@ -0,0 +1,19 @@
+package Algorithms;
+
+import java.io.*;
+import java.util.*;
+
+public class WarmUp_1 {
+    public static void solveMeFirst(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        solveMeFirst(a, b);
+    }
+}
